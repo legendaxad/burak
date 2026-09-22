0d6b1f07419412b71d96466b43d000304a1b8fad
Kyle <abdulaxadjon9@gmail.com>
2026-09-22 15:43:50 +0900

    feat:Connect mongoDb via mongoose

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,14 @@
 import dotenv from "dotenv";
-
+import mongoose from "mongoose";
 dotenv.config();
-
-console.log(process.env.MONGO_URL);
+mongoose.set("strictQuery", false);
+mongoose
+  .connect(process.env.MONGO_URL as string)
+  .then(() => {
+    console.log("MongoDB connected");
+    const port = process.env.PORT || 3000;
+    console.log(`Server is running on port ${port}`);
+  })
+  .catch((err) => {
+    console.log("error:", err);
+  });
