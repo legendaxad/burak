0427c3044738409ad445d11b6265e71c3b8b539a
Kyle <abdulaxadjon9@gmail.com>
2026-09-25 11:53:10 +0900

    feat:develop member schema

---
+++ src/libs/enums/member.enums.ts
@@ -0,0 +1,10 @@
+export enum MemberType {
+  ADMIN = "ADMIN",
+  USER = "USER",
+  RESTAURANT = "RESTAURANT",
+}
+export enum MemberStatus {
+  ACTIVE = "ACTIVE",
+  BLOCK = "BLOCK",
+  DELETE = "DELETE",
+}
---
+++ src/schema/Member.model.ts
@@ -0,0 +1,49 @@
+import mongoose, { Schema } from "mongoose";
+import { MemberStatus, MemberType } from "../libs/enums/member.enums";
+
+const memberSchema = new Schema(
+  {
+    memberType: {
+      type: String,
+      enum: MemberType,
+      default: MemberType.USER,
+    },
+    memberStatus: {
+      type: String,
+      enum: MemberStatus,
+      default: MemberStatus.ACTIVE,
+    },
+    memberNick: {
+      type: String,
+      index: { unique: true, sparse: true },
+      required: true,
+    },
+    memberPhone: {
+      type: String,
+      index: { unique: true, sparse: true },
+      required: true,
+    },
+    memberPassword: {
+      type: String,
+      required: true,
+      select: false,
+    },
+    memberAdress: {
+      type: String,
+      required: true,
+    },
+    memberDesc: {
+      type: String,
+    },
+    memberImage: {
+      type: String,
+    },
+    memberPoint: {
+      type: Number,
+      default: 0,
+    },
+  },
+  { timestamps: true },
+);
+
+export default mongoose.model("Member", memberSchema);
