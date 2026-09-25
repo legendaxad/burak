9369d6c3a72a3dc0fad41fe54a1e7a1661c77fab
Kyle <abdulaxadjon9@gmail.com>
2026-09-25 12:10:33 +0900

    fix:Task is complited

--- src/train.ts
+++ src/train.ts
@@ -1,26 +1,50 @@
+// N-TASK
+
+// const palindromCheck = (str: string): boolean => {
+//   const reversedStr = str.split("").reverse().join("");
+//   return str === reversedStr;
+// };
+
+// console.log(palindromCheck("dad")); // true
+// console.log(palindromCheck("son")); // false
+
+// const palindromeCheck = (str: string) => {
+//   let result = "";
+
+//   for (let i = 0; i < str.length; i++) {
+//     result = str[i] + result;
+//   }
+
+//   console.log("before:", str);
+//   console.log("after:", result);
+//   console.log("Result:", str === result);
+// };
+
+// palindromeCheck("dad");
+
 // M-TASK
 
-interface type_array {
-  number: number;
-  square: number;
-}
+// interface type_array {
+//   number: number;
+//   square: number;
+// }
 
-const getSquareNumbers = (array: number[]): type_array[] => {
-  const result: type_array[] = [];
-  let i = 0;
-  for (i; i < array.length; i++) {
-    let num = array[i];
-    let square = num * num;
+// const getSquareNumbers = (array: number[]): type_array[] => {
+//   const result: type_array[] = [];
+//   let i = 0;
+//   for (i; i < array.length; i++) {
+//     let num = array[i];
+//     let square = num * num;
 
-    let obj: type_array = {
-      number: num,
-      square: square,
-    };
+//     let obj: type_array = {
+//       number: num,
+//       square: square,
+//     };
 
-    result[i] = obj;
-  }
+//     result[i] = obj;
+//   }
 
-  return result;
-};
+//   return result;
+// };
 
-console.log(getSquareNumbers([1, 2, 3]));
+// console.log(getSquareNumbers([1, 2, 3]));
